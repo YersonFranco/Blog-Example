e0287ec6aef2d82c3e0a2417d322359bb179e014
YersonFranco <yersonsneyder@gmail.com>
2016-04-07 11:31:53 -0500

    Pactice Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,24 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  resources :articles
+    get "/articles"
+    post "/articles"
+    delete "/articles"
+    get "/articles/:id"
+    get "/articles/new"
+    get "articles/:id/edit"
+    patch "/articles/:id"
+    put "/articles/:id"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'welcome#index'                
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  #   get 'products/:id' => 'catalog#view'                                                                                                    
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
+  # Example of named route that can be invoked with purchase_url(id: product.id)                  
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
