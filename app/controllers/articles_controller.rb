7c44d698d0464f0ccf33c9a185f790b400921f64
YersonFranco <yersonsneyder@gmail.com>
2016-04-08 17:18:43 -0500

    Practice Private class, Active record

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new(title: params[:article][:title], body: params[:article][:body])
+		@article = Article.new(article_params)
 
 		if @article.save
 			redirect_to @article
@@ -20,6 +20,21 @@ class ArticlesController < ApplicationController
 			render :new
 		end
 		
+	end
+
+	def destroy
+	@article = Article.find(params[:id])
+	@article.destroy
+	redirect_to articles_path
+	end
+
+	def update
+		
+	end
+
+	private
+	def article_params
+		params.require(:article).permit(:title,:body)
 	end
 
 end	
